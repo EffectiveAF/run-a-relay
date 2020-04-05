1446bf1228bca54a4926581c8b4e489d65e43958
Tim Sullivan <timsullivan97@gmail.com>
2020-04-05 04:00:03 -0700

    adjust header spacing and buttons

--- src/App.svelte
+++ src/App.svelte
@@ -10,25 +10,39 @@
     flex-direction: row;
     justify-content: left;
     align-items: center;
-    border-bottom: 1px solid #f0f0f0;
+    border-bottom: 1px solid var(--neutral-100);
+    padding: 0px 16px;
+    height: 64px;
   }
+  
   .nav-right {
+    display: flex;
     margin-left: auto;
   }
-  .nav-right a {
-    background-color: purple;
+
+  .nav-right button {
+    margin-left: 16px;
+  }
+
+  img {
+    max-width: 32px;
+    max-height: 32px;
+    margin-right: 8px;
   }
 </style>
 
 <Router>
   <nav>
+
+    <Link to="/"><img src="/img/tor.svg" alt="Tor-esque logo (onion)" /></Link>
+
     <h4>Run A Relay</h4>
     <div class="nav-right">
       <a href="https://github.com/EffectiveAF/run-a-relay#faq" target="_blank" rel="nofollow noreferrer noopener">
-        <button>RaR FAQ</button>
+        <button class="secondary hvr-ripple-out-sec">RaR FAQ</button>
       </a>
       <a href="https://donate.torproject.org/" target="_blank" rel="nofollow noreferrer noopener">
-        <button>Donate to Tor</button>
+        <button class="primary hvr-ripple-out">Donate to Tor</button>
       </a>
     </div>
   </nav>
