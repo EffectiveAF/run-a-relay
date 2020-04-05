541d2cdb924625406bd84cdde95656b8f44c12dd
Steve Phillips <steve@tryingtobeawesome.com>
2020-04-05 06:49:54 -0700

    Created and used Nav, ExternalLink, and ExternalButton components; centered button text; centered logo (display: block)

---
+++ src/ExternalButton.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let href = '#';
+
+  import ExternalLink from './ExternalLink.svelte';
+</script>
+
+<ExternalLink href={href}>
+  <button class="primary hvr-ripple-out">
+    <slot />
+  </button>
+</ExternalLink>
---
+++ src/ExternalLink.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let href = '#';
+</script>
+
+<a href={href} target="_blank" rel="nofollow noreferrer noopener">
+  <slot />
+</a>
