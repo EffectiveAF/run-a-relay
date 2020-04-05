a7f553ac0c0e6cafd5914ddfe0a370a89ddef1ad
Steve Phillips <steve@tryingtobeawesome.com>
2020-04-05 06:51:04 -0700

    Created and used relatively ugly About component

---
+++ src/About.svelte
@@ -0,0 +1,32 @@
+<script>
+  import ExternalLink from './ExternalLink.svelte';
+</script>
+
+<style>
+  .about {
+    padding: 40px;
+    font-size: 18px;
+  }
+  ul {
+    margin-left: 30px;
+  }
+  li, p {
+    margin-top: 10px;
+  }
+</style>
+
+<div class="about">
+  <h2>Who Built <em>Run A Relay</em>?</h2>
+
+  <ul>
+    <li>- Steve Phillips / <ExternalLink href="https://github.com/elimisteve">@elimisteve</ExternalLink>, privacy activist and Founder of <ExternalLink href="https://effective.af/">EffectiveAF</ExternalLink></li>
+    <ul>
+      <li>- Lead developer and hoster of <ExternalLink href="https://leapchat.org">LeapChat</ExternalLink>: end-to-end encrypted, ephemeral chat in the browser</li>
+    </ul>
+    <li>- Tim Sullivan / <ExternalLink href="https://github.com/timsully">@timsully</ExternalLink>, UI/human factors engineer at Splunk</li>
+  </ul>
+
+  <p>
+    We love Tor!
+  </p>
+</div>
--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,13 @@
 
   import Nav from './Nav.svelte';
   import Home from './Home.svelte';
+  import About from './About.svelte';
 </script>
 
 <Router>
   <Nav />
   <div>
     <Route path="/"><Home /></Route>
+    <Route path="/about"><About /></Route>
   </div>
 </Router>
