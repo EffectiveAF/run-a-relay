2b473b38ad4eb65b646de57bf9bc4ad009cf5ea0
Steve Phillips <steve@tryingtobeawesome.com>
2020-04-07 15:58:11 -0700

    Added nav keyboard shortcuts! Fixes #10

--- src/PrevNextStepButtons.svelte
+++ src/PrevNextStepButtons.svelte
@@ -3,6 +3,18 @@
 
   import { currentStepIndex } from './stores.js';
   import slugOrder from './slugOrder.js';
+
+  const navPrevious = () => {
+    $currentStepIndex--;
+    const newSlug = slugOrder[$currentStepIndex];
+    navigate(`/step/${newSlug}`);
+  }
+
+  const navNext = () => {
+    $currentStepIndex++;
+    const newSlug = slugOrder[$currentStepIndex];
+    navigate(`/step/${newSlug}`);
+  }
 </script>
 
 <style>
@@ -24,25 +36,48 @@
   }
 </style>
 
-<svelte:window on:popstate={(e) => {
-  // Re-sync URL displayed and currentStepIndex
-  const stepName = document.location.pathname
-        .slice('/step/'.length)
-        .match(/[\w\-]+/)[0];
-  const stepIndex = slugOrder.indexOf(stepName);
-  if (stepIndex !== -1) {
-    $currentStepIndex = stepIndex;
-  }
-}} />
+<svelte:window
+  on:popstate={(e) => {
+    // Re-sync URL displayed and currentStepIndex
+    const maybeStepName = document.location.pathname
+          .slice('/step/'.length)
+          .match(/[\w\-]+/);
+    if (!maybeStepName || maybeStepName.length !== 1) {
+      return;
+    }
+    const stepName = maybeStepName[0];
+
+    const stepIndex = slugOrder.indexOf(stepName);
+    if (stepIndex !== -1) {
+      $currentStepIndex = stepIndex;
+    }
+  }}
+
+  on:keydown={(e) => {
+    if (e.target.tagName !== 'BODY') {
+      // Don't trigger keyboard shortcuts in code <input>s!
+      return;
+    }
+
+    if ($currentStepIndex > 0) {
+      if (['ArrowLeft', 'ArrowUp'].indexOf(e.key) !== -1) {
+        navPrevious();
+        return;
+      }
+    }
+    if ($currentStepIndex < slugOrder.length - 1) {
+      if (['ArrowRight', 'ArrowDown'].indexOf(e.key) !== -1) {
+        navNext();
+        return;
+      }
+    }
+  }}
+/>
 
 <div class="prev-next">
   <div class="prev-ctn">
     {#if $currentStepIndex > 0}
-      <button class="secondary" on:click={() => {
-        $currentStepIndex--;
-        const newSlug = slugOrder[$currentStepIndex];
-        navigate(`/step/${newSlug}`);
-      }}>
+      <button class="secondary" on:click={navPrevious}>
         Previous
       </button>
     {/if}
@@ -50,11 +85,7 @@
 
   <div class="next-ctn">
     {#if $currentStepIndex < slugOrder.length - 1}
-      <button class="primary" on:click={() => {
-        $currentStepIndex++;
-        const newSlug = slugOrder[$currentStepIndex];
-        navigate(`/step/${newSlug}`);
-      }}>
+      <button class="primary" on:click={navNext}>
         Next
       </button>
     {/if}
