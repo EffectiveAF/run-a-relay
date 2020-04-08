86e4bbe2215e1968ef9b45b92da775501eeab18e
Steve Phillips <steve@tryingtobeawesome.com>
2020-04-08 04:44:33 -0700

    Added collapsing nav-right on mobile!

--- src/Nav.svelte
+++ src/Nav.svelte
@@ -2,6 +2,43 @@
   import { Link } from 'svelte-routing';
 
   import ExternalButton from './ExternalButton.svelte';
+
+  // Derived from https://stackoverflow.com/a/28344281/197160
+  function _hasClass(el, cls) {
+    return !!el.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
+  }
+  function _addClass(el, cls) {
+    if (!_hasClass(el, cls)) {
+      el.className += ' ' + cls;
+    }
+  }
+  function _removeClass(el, cls) {
+    if (_hasClass(el,cls)) {
+      var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
+      el.className = el.className.replace(reg, '');
+    }
+  }
+
+  const togglerLabelOpen = 'V';
+  const togglerLabelClose = 'X';
+
+  // Styling in public/relay.css
+  const _toggleClass = 'show-right-nav-mobile';
+
+  const toggleNavRight = (e) => {
+    const navRight = document.getElementsByClassName('nav-right')[0];
+    const toggler = document.getElementsByClassName('toggle-nav-right')[0];
+
+    if (_hasClass(navRight, _toggleClass)) {
+      navRight.style.visibility = 'hidden';
+      _removeClass(navRight, _toggleClass);
+      toggler.textContent = togglerLabelOpen;
+    } else {
+      navRight.style.visibility = 'visible';
+      _addClass(navRight, _toggleClass);
+      toggler.textContent = togglerLabelClose;
+    }
+  }
 </script>
 
 <style>
@@ -15,19 +52,17 @@
     height: 64px;
   }
 
-  @media (max-width: 640px) {
-    nav {
-      padding: 0 10px;
-    }
-  }
-
   nav a:hover {
     text-decoration: none;
   }
 
-  .nav-right {
+  .toggle-nav-right, .nav-right {
     display: flex;
     margin-left: auto;
+  }
+
+  .toggle-nav-right {
+    display: none;
   }
 
   nav img {
@@ -35,6 +70,24 @@
     max-width: 32px;
     max-height: 32px;
     margin-right: 8px;
+  }
+
+  @media (max-width: 640px) {
+    nav {
+      padding: 0 10px;
+    }
+
+    .toggle-nav-right {
+      display: flex;
+    }
+
+    .nav-right {
+      visibility: hidden;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      z-index: 2;
+    }
   }
 </style>
 
@@ -60,4 +113,15 @@
       Donate to Tor
     </ExternalButton>
   </div>
+
+  <button
+    class="toggle-nav-right"
+    on:click={toggleNavRight}
+    on:blur={() => {
+      // TODO: Remove this race condition hack
+      setTimeout(toggleNavRight, 250);
+    }}
+  >
+    {togglerLabelOpen}
+  </button>
 </nav>
