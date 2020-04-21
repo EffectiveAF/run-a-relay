d7828a77ac1157b023a73857b5e0486194c3cbff
Steve Phillips <steve@tryingtobeawesome.com>
2020-04-20 19:50:56 -0700

    Added ssh/ -- rar2txt.py (to generate most of new steps.txt), handler.sh, init.txt

---
+++ ssh/rar2txt.py
@@ -0,0 +1,22 @@
+import re
+
+rar_orig = open('../src/Steps.svelte').read()
+
+on_step = 1
+
+print('# RunARelay.org Tutorial')
+
+for title, p in re.findall(r'(?:title="(.*?)")|(?:<p>(.*?)</p>)', rar_orig, re.DOTALL):
+    if title:
+      print('\n## Step ' + str(on_step) + ':', title + '\n')
+      on_step += 1
+
+    if p and '<button' not in p and '{@html' not in p:
+      p = re.sub(r'\n +', '\n ', p.lstrip())
+      p = re.sub(r'<ExternalLink href="(.*?)">\s*(.*?)\s*</ExternalLink>', r'\2 ( \1 )', p)
+      print(p.
+            replace('&nbsp;', ' ').
+            replace('  ', ' ').
+            replace('<em>', '**').
+            replace('</em>', '**')
+      )
