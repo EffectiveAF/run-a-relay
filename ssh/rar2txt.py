868ee0dfd895d9232dc07dee33dd13e9ae56b062
Steve Phillips <steve@tryingtobeawesome.com>
2020-04-20 20:06:30 -0700

    ssh/steps.txt: Improved formatting ("---" between steps)

--- ssh/rar2txt.py
+++ ssh/rar2txt.py
@@ -8,6 +8,8 @@ print('# RunARelay.org Tutorial')
 
 for title, p in re.findall(r'(?:title="(.*?)")|(?:<p>(.*?)</p>)', rar_orig, re.DOTALL):
     if title:
+      if on_step > 1:
+          print('---')
       print('\n## Step ' + str(on_step) + ':', title + '\n')
       on_step += 1
 
