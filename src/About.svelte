911c1ab276fda788a62188b754c049dd8071e8ba
Tim Sullivan <timsullivan97@gmail.com>
2020-05-01 23:18:26 -0700

    add about page elements

--- src/About.svelte
+++ src/About.svelte
@@ -5,20 +5,103 @@
 </script>
 
 <style>
+.flex-container {
+  display: flex;
+}
+
+.flex-child {
+  flex: 1;
+}
+
+.flex-child:first-child {
+  margin-right: 48px;
+}
+
+.h3-padding {
+  padding: 56px 0px 8px 0px;
+}
+
+.padding-top {
+  margin-top: 104px;
+}
+
+div > img:first-child {
+  margin-right: 64px;
+}
+
+div > img {
+  border-radius: 4px;
+}
+
+.names {
+  font-family: Open Sans;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  padding-top: 4px;
+  display: flex;
+  justify-content: center;
+  width: 346px;
+}
+
+.names:first-child {
+  margin-right: 64px;
+}
+
+.align-center {
+  display: flex;
+  justify-content: center;
+  width: 346px;
+  padding-top: 8px;
+}
 </style>
 
-<div class="static-content">
-  <h3>Who Built <em>Run A Relay</em>?</h3>
 
-  <ul>
-    <li>- Steve Phillips / <ExternalLink href="https://github.com/elimisteve">@elimisteve</ExternalLink>, privacy activist and Founder of <ExternalLink href="https://effective.af/">EffectiveAF</ExternalLink></li>
-    <ul>
-      <li>- Lead developer and hoster of <ExternalLink href="https://leapchat.org">LeapChat</ExternalLink>: end-to-end encrypted, ephemeral chat in the browser</li>
-    </ul>
-    <li>- Tim Sullivan / <ExternalLink href="https://github.com/timsully">@timsully</ExternalLink>, Design Technologist at Splunk</li>
-  </ul>
+<div class="container-fluid flex-container">
 
-  <p>
-    We love Tor!
-  </p>
+  <div class="flex-child">
+
+    <div class="col-lg-7 col-lg-offset-2">
+      <h1 class="padding-top">Creating a better future, together.</h1>
+    </div>
+
+
+    <div class="col-lg-offset-2">
+      <div class="col-lg-6">
+        <h3 class="col-lg-4 h3-padding">Who we are</h3>
+      </div>
+
+      <p class="col-lg-7">It would make sense to introduce in this body of text what this is and how to get started. The rest of this text is.</p>
+
+      <div class="col-lg-6"><h3 class="col-lg-4 h3-padding">Our Mission</h3></div>
+      
+      
+      <p class="col-lg-7">It would make sense to introduce in this body of text what this is and how to get started. The rest of this text is.
+      </p>
+    </div>
+
+  </div>
+
+
+  <div class="flex-child">
+
+    <div class="row padding-top">
+      
+      <div>
+        <img src="../img/steve.png" width="346" height="504" alt="Image of Steve Phillips">
+        <h5 class="names">Steve Phillips</h5>
+        <h6 class="align-center">Founder of Effective.AF</h6>
+      </div>
+
+      <div>
+        <img src="../img/tim.png" width="346" height="504" alt="Image of Tim Sullivan">
+        <h5 class="names">Tim Sullivan</h5>
+        <h6 class="align-center">Design Technologist</h6>
+      </div>
+    </div>
+
+    <!-- second flex child endpoint -->
+  </div>
+
+  <!-- container endpoint -->
 </div>
