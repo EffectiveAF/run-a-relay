0df0f3a7fce49e4c903157aa9bcbe0ee362ec21d
Tim Sullivan <timsullivan97@gmail.com>
2020-05-01 19:06:50 -0700

    test grid on homepage

--- src/Home.svelte
+++ src/Home.svelte
@@ -13,26 +13,35 @@
 
 <style>
   .home {
-    display: flex;
-    flex-direction: column;
+    /* display: flex; */
+    /* flex-direction: row; */
     justify-content: center;
     align-items: center;
-    margin-top: 30px;
+    margin-top: 160px;
   }
   h1, h2 {
-    text-align: center;
+    text-align: left;
   }
   h2 {
-    margin-top: 40px;
-    margin-bottom: 40px;
+    margin: 24px 0px;
+  }
+  p {
+    margin-bottom: 24px;
   }
 </style>
 
-<div class="home">
-  <h1>Thinking about running a Tor relay?</h1>
-  <h2>You're in the right place!</h2>
+<div class="container-fluid">
+
+  <div class="home col-lg-offset-2">
+    <div class="row col-lg-6"><h1 class="hero-header">Thinking about running a Tor relay?</h1></div>
+    
+    <div class="row"><h2 class="col-lg-4">What is this?</h2></div>
+    
+    <div class="row"><p class="col-lg-4">It would make sense to introduce in this body of text what this is and how to get started. The rest of this text is lorem ipsum suspendisse ante urna, condimentum vel diam eget, condimentum convallis elit. Maecenas congue enim quis tincidunt fermentum. Sed sit amet neque sapien. Donec ac nunc tincidunt, sodales dolor sit amet, egestas diam.
+    </p></div>
+  </div>
 
   <Link to="/step/intro">
-    <button class="primary hvr-ripple-out">Get Started</button>
+    <button class="primary hvr-ripple-out col-lg-offset-2">Get Started</button>
   </Link>
 </div>
